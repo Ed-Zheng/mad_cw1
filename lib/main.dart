02f455f861d7d5fdcb06181aef331eefac537779
Edison Zheng <sketchyazx@gmail.com>
2025-09-18 16:51:48 -0400

    Implements dark/light mode toggle and spaces code out for readability
    Changes class MyApp to a stateful widget, creates a boolean variable for tracking the theme state, sets the theme according to the boolean, and adds a button that toggles the theme boolean

--- lib/main.dart
+++ lib/main.dart
@@ -4,24 +4,46 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // Boolean variable to track theme state
+  bool _darkMode = false;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 255, 170, 0)),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 255, 170, 0),
+          brightness: _darkMode ? Brightness.dark : Brightness.light,
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+
+      home: MyHomePage(
+        title: 'Flutter Demo Home Page',
+        toggleTheme: () {
+          setState(() {
+            _darkMode = !_darkMode; // toggles the boolean to swap themes
+          });
+        },
+      ),
+
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({super.key, required this.title, required this.toggleTheme});
   final String title;
+  final VoidCallback toggleTheme;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -39,53 +61,69 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      title: Text(widget.title),
-    ),
-    body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          const Text('You have pushed the button this many times:'),
-          Text(
-            '$_counter',
-            style: Theme.of(context).textTheme.headlineMedium,
-          ),
-
-          const SizedBox(height: 20), // spacing
-
-          // Images
-          Image.network(
-            _firstImage
-              ? 'https://minifigs.me/cdn/shop/files/Monkey.jpg?v=1709909553'
-              : 'https://content.instructables.com/FUE/1173/HXIKE6RJ/FUE1173HXIKE6RJ.jpg?auto=webp&crop=1.2%3A1&frame=1&width=270',
-            width: 150,
-            height: 150,
-          ),
-
-          const SizedBox(height: 20), // spacing
-
-          ElevatedButton(
-            onPressed: () {
-              setState(() {
-                  _firstImage = !_firstImage; // toggles the boolean to swap images
-                });
-            },
-            child: const Text("Toggle Image"),
-          ),
-        ],
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
       ),
-    ),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+            const Text('You have pushed the button this many times:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+
+            const SizedBox(height: 20), // spacing
+
+            // Images
+            Image.network(
+              _firstImage
+                  ? 'https://minifigs.me/cdn/shop/files/Monkey.jpg?v=1709909553'
+                  : 'https://content.instructables.com/FUE/1173/HXIKE6RJ/FUE1173HXIKE6RJ.jpg?auto=webp&crop=1.2%3A1&frame=1&width=270',
+              width: 150,
+              height: 150,
+            ),
+
+            const SizedBox(height: 20), // spacing
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _firstImage = !_firstImage; // toggles the boolean to swap images
+                    });
+                  },
+                  child: const Text("Toggle Image"),
+                ),
+
+                const SizedBox(width: 10), // spacing
+
+                ElevatedButton(
+                  onPressed: widget.toggleTheme,
+                  child: const Text("Toggle Theme"),
+                ),
+
+              ],
+            ),
+          ],
+        ),
+      ),
 
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
+
     );
   }
 }
