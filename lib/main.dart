6931b905f72aa3e373164e0baa98825e6e0a1011
Edison Zheng <sketchyazx@gmail.com>
2025-09-19 15:05:30 -0400

    Animates images when toggled
    Puts image widget inside of an animated container to add an enlarging and shrinking animation with a curve that starts slow and ends fast

--- lib/main.dart
+++ lib/main.dart
@@ -81,14 +81,18 @@ class _MyHomePageState extends State<MyHomePage> {
 
             const SizedBox(height: 20), // spacing
 
+          AnimatedContainer(
+            curve: Curves.easeInCubic,
+            duration: Duration(seconds: 1),
+            width: _firstImage ? 125 : 300,
+            height: _firstImage ? 125 : 300,
             // Images
-            Image.network(
+            child: Image.network(
               _firstImage
                   ? 'https://minifigs.me/cdn/shop/files/Monkey.jpg?v=1709909553'
                   : 'https://content.instructables.com/FUE/1173/HXIKE6RJ/FUE1173HXIKE6RJ.jpg?auto=webp&crop=1.2%3A1&frame=1&width=270',
-              width: 150,
-              height: 150,
             ),
+          ),
 
             const SizedBox(height: 20), // spacing
 
